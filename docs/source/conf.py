302db18689b8e9a373c42a57d19174daf1b7a7a1
Salvatore Greco <45233784+grecosalvatore@users.noreply.github.com>
2024-02-01 23:58:14 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DriftLens'
+copyright = '2024, Salvatore'
+author = 'Salvatore'
 
 release = '0.1'
 version = '0.1.0'
